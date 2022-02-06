53c9e169ea4512ae46a265a45c3c480007307f2d
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-02-06 13:42:00 -0500

    added basic fetch call for foursquare API

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var query;
+var lat;
+var lon;
+requestUrl =
+  "https://api.foursquare.com/v3/places/search?query=" +
+  query +
+  "&ll=" +
+  lat +
+  "%2C" +
+  lon;
+
+var options = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+    Authorization: "fsq3a28437clLV4OGd1TqTQpoGeqZglCHRplaWmbBfL4UzY=",
+  },
+};
+
+fetch(requestUrl, options)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
